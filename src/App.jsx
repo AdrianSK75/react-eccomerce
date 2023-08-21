eff3aa6b808cbd5631fb9008bbe1eae4e0a02580
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2023-08-21 21:38:26 +0300

    todo-list created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Input, Button, Container } from '@chakra-ui/react'
+
 import './App.css'
+import { useState, useEffect } from 'react';
+
+const todoItems = [
+  {id: 1, todo: 'Alune'},
+  {id: 2, todo: 'Prajituri'},
+  {id: 3, todo: 'Coarda'},
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(todoItems);
+  const [todo, setTodo] = useState("");
+  const [selectedTodo, setSelectedTodo] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (selectedTodo === null) {
+      setTodos((prevTodos) => [...prevTodos, { id: prevTodos.length + 1, todo }])
+    } else {
+      setTodos(
+        todos.map(todoItem => {
+          if (todoItem.id === selectedTodo.id) {
+            return {
+              ...todoItem,
+              todo
+            }
+          } else {
+            return todoItem
+          }
+        })
+      )
+    }
+    setSelectedTodo(null)
+    setTodo('');
+  }
+
+  const handleDelete = (todoId) => {
+    setTodos(
+      todos.filter(todo => todo.id !== todoId)
+    )
+  }
+
+  useEffect(() => console.log(selectedTodo));
+
+  // const value
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form
+        className='container'
+        onSubmit={handleSubmit}>
+        <Input
+          className='todo-input'
+          variant='flushed'
+          type='text'
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <Button
+          colorScheme='teal'
+          size='md'
+          type='submit'
+        >
+          {selectedTodo === null ? 'New' : 'Edit'}
+        </Button>
+      </form>
+      <ul className='todo-list'>
+        {todos.map((todoItem) => (
+          <li key={todoItem.id}>
+            {todoItem.todo}
+            <Button onClick={() => {
+              setSelectedTodo(todoItem)
+              setTodo(todoItem.todo)
+              }}> Edit </Button>
+            <Button onClick={() => handleDelete(todoItem.id)}> Delete </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App
